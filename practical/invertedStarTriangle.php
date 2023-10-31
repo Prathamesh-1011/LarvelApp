3cc751440d2b6f69a1723093ef44d8c559243e16
Prathamesh Rokade <prathameshr955@gmail.com>
2023-10-31 21:16:33 +0530

    Updated Inverted Star Triangle program for readability and maintainability

--- practical/invertedStarTriangle.php
+++ practical/invertedStarTriangle.php
@@ -1,15 +1,25 @@
 <?php
 
-$rows = 5; 
-
-for ($i = $rows; $i >= 1; $i--) {
-
-    for ($j = 1; $j <= $i; $j++) {
-        echo "* ";
+// Function to print a left-sided inverted star triangle
+function printLeftSidedInvertedStarTriangle($height) {
+    // Loop to iterate through each row
+    for ($i = $height; $i >= 1; $i--) {
+        // Loop to print stars for the current row
+        for ($k = 1; $k <= $i; $k++) {
+            echo "*";
+        }
+        // Move to the next line for the next row
+        echo "\n";
     }
-
-    echo "\n";
 }
+
+// Define the height of the left-sided inverted triangle
+$height = 5;
+
+// Call the printLeftSidedInvertedStarTriangle function
+printLeftSidedInvertedStarTriangle($height);
+
 ?>
+
 
 
