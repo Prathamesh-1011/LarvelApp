0dc8710af0285fa83bb674ea45215713bee2b4c3
Prathamesh Rokade <prathameshr955@gmail.com>
2023-11-06 19:43:00 +0530

    Updated test cases for song class

--- tests/Unit/SongTest.php
+++ tests/Unit/SongTest.php
@@ -9,17 +9,17 @@ use Practical\Song;
 
 
 class SongTest extends TestCase {
-    // public function testSetTempoAcceptsInteger(): void {
-    //     $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
-    //     $song->setTempo(120);
-    //     $this->assertEquals(120, $song->getTempo());
-    // }
+    public function testSetTempoAcceptsInteger(): void {
+        $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
+        $song->setTempo(120);
+        $this->assertEquals(120, $song->getTempo());
+    }
 
-    // public function testSetTempoRejectsNonNumeric(): void {
-    //     $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
-    //     $this->expectException(\InvalidArgumentException::class);
-    //     $song->setTempo("NotANumber");
-    // }
+    public function testSetTempoRejectsNonNumeric(): void {
+        $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
+        $this->expectException(\InvalidArgumentException::class);
+        $song->setTempo("NotANumber");
+    }
     public function testSetTempo(): void {
         $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
         $song->setTempo(60);
@@ -44,9 +44,6 @@ class SongTest extends TestCase {
         $song->setTempo(60.5);
     }
 
-    // public function testGenerateFibonacciRejectsFloat(): void {
-    //     $this->expectException(\InvalidArgumentException::class);
-    //     Practical::generateFibonacciSequence(3.5);
 
 }
 ?>
