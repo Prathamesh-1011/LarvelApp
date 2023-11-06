e2186d1bf15291bf49cff9d44822289c03ee6f7e
Prathamesh Rokade <prathameshr955@gmail.com>
2023-11-06 19:54:20 +0530

    Created routes for our veggies,added test cases for it in DemoTest and created a simple web preview

---
+++ resources/views/veggies.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PNP Vegetable Shop</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        table {
+            width: 50%;
+            margin: 0 auto;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #333;
+            color: #fff;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        tr:nth-child(odd) {
+            background-color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <h1>PNP Vegetable Shop</h1>
+    <table border="1">
+        <tr>
+            <th>Vegetable</th>
+            <th>Price (per kilogram)</th>
+            
+        </tr>
+        <tr>
+            <td>Aaloo</td>
+            <td>$1.99</td>
+        </tr>
+        <tr>
+            <td>Bhindi</td>
+            <td>$2.49</td>
+        </tr>
+        <tr>
+            <td>Baigan</td>
+            <td>$1.79</td>
+        </tr>
+        <tr>
+            <td>Gobhi</td>
+            <td>$1.29</td>
+        </tr>
+        <tr>
+            <td>Spinach</td>
+            <td>$2.99</td>
+        </tr>
+    </table>
+</body>
+</html>
