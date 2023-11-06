8e24c915c5cfe9e851739bc0916a8100ffb6862b
Prathamesh Rokade <prathameshr955@gmail.com>
2023-11-06 19:40:35 +0530

    Add test cases for song class

---
+++ tests/Unit/SongTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+require(__DIR__.'/../../Practical/song.php');
+use Practical\Song;
+
+
+class SongTest extends TestCase {
+    // public function testSetTempoAcceptsInteger(): void {
+    //     $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
+    //     $song->setTempo(120);
+    //     $this->assertEquals(120, $song->getTempo());
+    // }
+
+    // public function testSetTempoRejectsNonNumeric(): void {
+    //     $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
+    //     $this->expectException(\InvalidArgumentException::class);
+    //     $song->setTempo("NotANumber");
+    // }
+    public function testSetTempo(): void {
+        $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
+        $song->setTempo(60);
+        $this->assertEquals(60, $song->getTempo());
+    }
+
+    public function testSetTempoAcceptsIntegerString(): void {
+        $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
+        $song->setTempo("60");
+        $this->assertEquals(60, $song->getTempo());
+    }
+
+    public function testSetTempoRejectsAlphabetString(): void {
+        $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
+        $this->expectException(\InvalidArgumentException::class);
+        $song->setTempo("60 BPM");
+    }
+
+    public function testSetTempoRejectsFloat(): void {
+        $song = new Song("Test Song", "Test Artist", "Test Genre", 100);
+        $this->expectException(\InvalidArgumentException::class);
+        $song->setTempo(60.5);
+    }
+
+    // public function testGenerateFibonacciRejectsFloat(): void {
+    //     $this->expectException(\InvalidArgumentException::class);
+    //     Practical::generateFibonacciSequence(3.5);
+
+}
+?>
